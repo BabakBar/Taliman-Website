288f7f9fced481b9f0339504b193226ae3ac83e6
BabakBar <babak.barghi@gmail.com>
2025-09-02 21:52:57 +0200

    cleanup

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function RootPage() {
-  // This page only renders when the user visits `/` without a locale prefix
-  // The middleware will handle the redirect to the default locale
-  redirect('/fa');
-}
